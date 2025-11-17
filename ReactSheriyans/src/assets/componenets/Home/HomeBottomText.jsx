20433b5f1c8831dd5f1a30b7359c68ff07cafc18
Rounakazad <143435829+Rounakazad@users.noreply.github.com>
2025-11-17 16:58:35 +0000

    Update components and fix React Link error

--- ReactSheriyans/src/assets/componenets/Home/HomeBottomText.jsx
+++ ReactSheriyans/src/assets/componenets/Home/HomeBottomText.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+
 
 const HomeBottomText = () => {
   return (
     <div>
+      <Link>Projects</Link>
       <Link>Projects</Link>
       
     </div>
