33fc0c29c46edcc477d088de031b65059a9a6d35
Rounak Azad <152050683+Rounakazadhere@users.noreply.github.com>
2025-11-23 06:25:08 +0000

    created project and agencess links

--- ReactSheriyans/src/assets/componenets/Home/HomeBottomText.jsx
+++ ReactSheriyans/src/assets/componenets/Home/HomeBottomText.jsx
@@ -4,9 +4,9 @@ import { Link } from "react-router-dom";
 
 const HomeBottomText = () => {
   return (
-    <div>
-      <Link>Projects</Link>
-      <Link>Projects</Link>
+    <div className='justify-center  gap-2 items-center flex font-[font500] text-white '>
+      <Link className='text-[6.5vw] border-2  leading-[5vw] pt-5 px-10 uppercase border-white rounded-full pb-2'>Projects</Link>
+      <Link className='text-[6.5vw] border-2  leading-[5vw] pt-5 px-10 uppercase border-white rounded-full pb-2'>Agencess</Link>
       
     </div>
   )
--- ReactSheriyans/src/assets/componenets/Home/HomeHeroText.jsx
+++ ReactSheriyans/src/assets/componenets/Home/HomeHeroText.jsx
@@ -3,10 +3,10 @@ import Video from './Video'
 
 const HomeHeroText = () => {
   return (
-    <div className='font-[font300] pt-7 text-center '>
-      <div className='uppercase text-[9.5vw] leading-[8vw] flex items-center justify-center'>L'étincelle</div>
-      <div className='uppercase text-[9.5vw] leading-[8vw] flex items-start justify-center'>qui<div className='h-[10vw] w-[19vw] -mt-3 -mb-1 rounded-full overflow-hidden'><Video /></div> génère</div>
-      <div className='uppercase text-[9.5vw] leading-[8vw] flex items-center justify-center'>la créativité</div>
+    <div className='font-[font300] pt-7 text-center text-white'>
+      <div className='uppercase text-[9.5vw] leading-[8vw] flex items-center justify-center pt-3'>L'étincelle</div>
+      <div className='uppercase text-[9.5vw] leading-[8vw] flex items-start justify-center pt-3'>qui<div className='h-[10vw] w-[19vw] -mt-3 -mb-1 rounded-full overflow-hidden '><Video /></div> génère</div>
+      <div className='uppercase text-[9.5vw] leading-[8vw] flex items-center justify-center pt-3'>la créativité</div>
     </div>
   )
 }
